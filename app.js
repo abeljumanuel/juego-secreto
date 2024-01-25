242fb8c59d46777b317600916d5f37c5ed8fddc4
abeljumanuel <abeljumanuel.rojas@gmail.com>
2024-01-25 01:36:09 -0500

    Versión final juego - Número Secreto
    Se implementaron mejoras para definir un rango de selección y un límite de intentos para lograr adivinar el número secreto.

---
+++ app.js
@@ -0,0 +1,80 @@
+let numeroSecreto = 0;
+let inferior = 1;
+let superior = 10;
+let intentos = 0;
+let listNumerosSorteados = [];
+let posibilidadesNumeros = (superior - inferior) + 1;
+
+function asignarTextoElemento(etiqueta, texto) {
+    let elementoHTML = document.querySelector(etiqueta);
+    elementoHTML.innerHTML = texto;
+}
+
+
+function verificarIntento(){
+    let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
+    intentos = parseInt(intentos);
+    console.log(` Intentos: ${intentos}\n Numero Secreto : ${numeroSecreto} \n Numeros Sorteados: ${listNumerosSorteados}\n Número Usuario: ${numeroUsuario}` )
+    console.log(typeof(intentos));
+    if (intentos > 2) {
+        console.log("El valor de intentos ya es mayor");
+        asignarTextoElemento('p', `El número secreto era ${numeroSecreto}, no pudiste averiguarlo en ${intentos} intento${intentos > 1 ? "s." : "."}`);
+        document.getElementById('intentar').setAttribute('disabled', true);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else if (numeroUsuario == numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} intento${intentos > 1 ? "s." : "."}`);
+        document.getElementById('intentar').setAttribute('disabled', true);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        // Usuario No acierta
+        if (numeroUsuario < listNumerosSorteados[listNumerosSorteados.length-1]) {
+            asignarTextoElemento('p', `El número secreto es mayor`);
+        } else {
+            asignarTextoElemento('p', `El número secreto es menor`);
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    console.log(listNumerosSorteados);
+}
+
+function generarNumeroSecreto(inferior, superior){
+    let numeroGenerado = Math.floor(Math.random() * (posibilidadesNumeros)) + inferior;
+
+    console.log(numeroGenerado);
+    console.log(listNumerosSorteados);
+
+    if (listNumerosSorteados.length == posibilidadesNumeros){
+       asignarTextoElemento('p', `Ya se sortearon todos los números posibles para el rango entre ${inferior} y ${superior}.`) 
+       document.getElementById('intentar').setAttribute('disabled', true);
+    } else {
+        if (listNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto(inferior, superior);
+        } else{
+            listNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto.');
+    asignarTextoElemento('p', `Indica un número de ${inferior} al ${superior}`);
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    document.getElementById('intentar').removeAttribute('disabled');
+    numeroSecreto = generarNumeroSecreto(inferior, superior);   
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    limpiarCaja();
+    condicionesIniciales();
+    
+}
+
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+}
+
+condicionesIniciales();
